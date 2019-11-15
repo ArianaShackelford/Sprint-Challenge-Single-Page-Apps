a463f448ce3309dddfbeb524696511ec4278c331
Ariana Shackelford <ariana.shackelford@gmail.com>
2019-11-15 13:07:24 -0500

    Created and styled character cards

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,46 @@
 import React from "react";
+import styled from 'styled-components';
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
+const Container = styled.div`
+display: flex;
+flex-direction:row;
+flex-wrap: wrap;
+justify-content:center;
+background:#2a3320 
+`;
+
+const Card= styled.div`
+ display:flex;
+ flex-direction: column;
+ width: 300px;
+ padding: 20px;
+ margin: 20px;
+ background: #ccf79e;
+ border-radius:5px;
+ align-items:center;
+`;
+
+const Img = styled.img`
+border-radius: 5px;
+margin: 5px;
+text-size: 25px;
+`;
+
+const Div = styled.div`
+margin: 5px;
+`;
+const CharacterCard = props => {
+  const {} = props.character;
+  return(
+
+    <Container> 
+      <Card className='character-card'>
+      <Div>{props.character.name}</Div>
+      <Img src={props.character.image}/>
+      <Div>Species: {props.character.species}</Div>
+      <Div>Status: {props.character.status}</Div>
+      </Card>
+    </Container>
+  )
 }
+export default CharacterCard;
