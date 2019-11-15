ecbe8e6b2bffeb006c87a3b3bc5a5c3831fbe826
Ariana Shackelford <ariana.shackelford@gmail.com>
2019-11-15 16:41:46 -0500

    search form in the works

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,10 +1,28 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import CharacterList from "./CharacterList";
 
-export default function SearchForm(props) {
+export default function SearchForm() {
+  const [searchTerm, setSearchTerm] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
+
+  useEffect(()=> {
+    const results = characters.filter(character =>
+      character.toLowerCase().includes(searchTerm.toLowerCase()))
+  },[searchTerm])
+
+  const handleChange = e => {
+    setSearchTerm(e.target.value)
+  }
+
+    return(<section className="search-form">
+     <form>
+       <label htmlFor='name'>Search:</label>
+       <input id='name'
+       type='text'
+       placeholder='find a character'
+       onChange={handleChange}
+       value={searchTerm}/>
+     </form>
+    </section>);
   
-  return (
-    <section className="search-form">
-     
-    </section>
-  );
 }
