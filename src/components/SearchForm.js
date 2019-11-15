fbc5a0c34d8b41ece2f8b664391b03fbf1aa8da1
Ariana Shackelford <ariana.shackelford@gmail.com>
2019-11-15 13:24:17 -0500

    Created search form

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,10 +1,26 @@
 import React, { useState } from "react";
 
 export default function SearchForm() {
- 
+  const [results, setResults] = useState();
+
+  const handleChanges = e => {
+    SpeechRecognitionResultList(e.target.value)
+  };
+
+  const submitHandler = e => {
+    e.preventDefault()}
+
+      const findCharacter = props.characters.filter(character => {
+        character.name.toLowerCase().indexOf(results.toLowerCase()) !== -1}
+    );
+        props.search(findCharacter);
+        console.log(findCharacter);
   return (
     <section className="search-form">
-     // Add a search form here
+     <form onSubmit={submitHandler}>
+       <input type='text' name='character' id='character' placeholder='character search' onChange={handleChanges}/>
+       
+     </form>
     </section>
   );
 }
